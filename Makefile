6a9c5162bb3d540abf1ff2d117da8625bb514db4
Jean Chassoul <chassoul@gmail.com>
2016-09-08 22:54:22 -0400

    so?

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 PROJECT = treehouse
 PROJECT_DESCRIPTION = The treehouse spontaneously generates imps, which are used to spawn your resources.
 PROJECT_VERSION = 0.0.1
-DEPS = cowboy chumak luerl gun lager uuid dht jsx msgpack jiffy
+DEPS = cowboy dht chumak luerl gun lager uuid jsx msgpack
+dep_cowboy = git https://github.com/ninenines/cowboy 1.1.x
 dep_dht = git https://github.com/jlouis/dht.git
+
 include erlang.mk
--- src/treehouse_app.erl
+++ src/treehouse_app.erl
@@ -6,12 +6,11 @@
 
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
-		{'_', [{"/", hello_handler, []}]}
-	]),
-	{ok, _} = cowboy:start_clear(http_listener, 100,
-			[{port, 8888}],
-			#{env => #{dispatch => Dispatch}}
-	),
+        {'_', [{"/", hello_handler, []}]}
+    ]),
+    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
 	treehouse_sup:start_link().
 
 stop(_State) ->
