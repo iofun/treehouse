a01b246e711ab1f79e81c2aac4c0b51ea5253f74
Jean Chassoul <chassoul@gmail.com>
2017-04-11 14:41:47 -0400

    ok

--- src/unit.erl
+++ src/unit.erl
@@ -8,11 +8,11 @@
 
 %% Management API.
 
-start(X, Y, St) ->
-    proc_lib:start(?MODULE, init, [X,Y,St]).
+start(X, Y, State) ->
+    proc_lib:start(?MODULE, init, [X,Y,State]).
 
-start_link(X, Y, St) ->
-    proc_lib:start_link(?MODULE, init, [X,Y,St]).
+start_link(X, Y, State) ->
+    proc_lib:start_link(?MODULE, init, [X,Y,State]).
 
 %% User API.
 
@@ -43,20 +43,20 @@ get_tc(Unit) ->
 set_unit(Unit, Name) ->				%Set a new unit chunk
     cast(Unit, {set_unit,Name}).
 
-lua_do(Unit, Cmd) ->				%"do" any Lua command
-    call(Unit, {lua_do,Cmd}).
+lua_do(Unit, Command) ->				%"do" any Lua command
+    call(Unit, {lua_do,Command}).
 
 gc(Unit) ->
     call(Unit, gc).
 
 %% Internal protocol functions.
 
-cast(Unit, Msg) ->
-    Unit ! {cast,self(),Msg},
+cast(Unit, Message) ->
+    Unit ! {cast,self(),Message},
     ok.
 
-call(Unit, Msg) ->
-    Unit ! {call,self(),Msg},
+call(Unit, Message) ->
+    Unit ! {call,self(),Message},
     receive
 	{reply,Unit,Rep} -> Rep
     end.
@@ -66,74 +66,72 @@ reply(To, Rep) ->
 
 %% Main loop.
 
-init(X, Y, St0) ->
+init(X, Y, State0) ->
     region:add_sector(X, Y, self()),		%Put us in the region
-    %% esdl_server:add_unit(),			%Add us to SDL
-    {_,St1} = luerl:call_function([this_unit,start], [], St0),
-    {_,St2} = luerl:call_function([this_unit,set_position], [X,Y], St1),
-    {_,St3} = luerl:call_function([this_unit,set_speed], [0,0], St2),
+    {_,State1} = luerl:call_function([this_unit,start], [], State0),
+    {_,State2} = luerl:call_function([this_unit,set_position], [X,Y], State1),
+    {_,State3} = luerl:call_function([this_unit,set_speed], [0,0], State2),
     proc_lib:init_ack({ok,self()}),
-    loop(St3, infinity, make_ref(), 0).		%Start with dummy tick ref
+    loop(State3, infinity, make_ref(), 0).		%Start with dummy tick ref
 
 %% loop(LuerlState, Tick, TickRef, TickCount) -> no_return().
 
-loop(St0, Tick, Tref, Tc) ->
+loop(State0, Tick, Tref, Tc) ->
     receive
 	tick ->
 	    %% Clock tick, move the unit.
-	    {_,St1} = luerl:call_function([this_unit,tick], [], St0),
+	    {_,State1} = luerl:call_function([this_unit,tick], [], State0),
 	    NewTref = erlang:send_after(Tick, self(), tick),
-	    loop(St1, Tick, NewTref, Tc+1);
+	    loop(State1, Tick, NewTref, Tc+1);
 	{cast,From,{set_tick,NewTick}} ->
 	    erlang:cancel_timer(Tref),		%Cancel existing timer
-	    {_,St1} = luerl:call_function([this_unit,set_tick], [NewTick], St0),
+	    {_,State1} = luerl:call_function([this_unit,set_tick], [NewTick], State0),
 	    %% Set the new tick and get a new timer
 	    NewTref = if NewTick =:= infinity ->
 			      make_ref();	%Dummy tick ref
 			 true ->
 			      erlang:send_after(NewTick, self(), tick)
 		      end,
-	    loop(St1, NewTick, NewTref, Tc);
+	    loop(State1, NewTick, NewTref, Tc);
 	{call,From,get_position} ->
-	    {[X,Y],St1} = luerl:call_function([this_unit,get_position], [], St0),
+	    {[X,Y],State1} = luerl:call_function([this_unit,get_position], [], State0),
 	    reply(From, {X,Y}),
-	    loop(St1, Tick, Tref, Tc);
+	    loop(State1, Tick, Tref, Tc);
 	{cast,From,{set_position,X,Y}} ->
-	    {_,St1} = luerl:call_function([this_unit,set_position],
-					  [float(X),float(Y)], St0),
-	    loop(St1, Tick, Tref, Tc);
+	    {_,State1} = luerl:call_function([this_unit,set_position],
+					  [float(X),float(Y)], State0),
+	    loop(State1, Tick, Tref, Tc);
 	{call,From,get_speed} ->
-	    {[Dx,Dy],St1} = luerl:call_function([this_unit,get_speed], [], St0),
+	    {[Dx,Dy],State1} = luerl:call_function([this_unit,get_speed], [], State0),
 	    reply(From, {Dx,Dy}),
-	    loop(St1, Tick, Tref, Tc);
+	    loop(State1, Tick, Tref, Tc);
 	{cast,From,{set_speed,Dx,Dy}} ->
-	    {_,St1} = luerl:call_function([this_unit,set_speed],
-					  [float(Dx),float(Dy)], St0),
-	    loop(St1, Tick, Tref, Tc);
+	    {_,State1} = luerl:call_function([this_unit,set_speed],
+					  [float(Dx),float(Dy)], State0),
+	    loop(State1, Tick, Tref, Tc);
 	{cast,From,zap} ->
-	    {_,_} = luerl:call_function([this_unit,zap], [], St0),
+	    {_,_} = luerl:call_function([this_unit,zap], [], State0),
 	    timer:sleep(1500),
 	    %% Remove ourselves from databases and die
-	    %% esdl_server:del_unit(),
 	    region:del_unit(),
 	    exit(zapped);
 	{call,From,get_state} ->		%Get the luerl state
-	    reply(From, {ok,St0}),
-	    loop(St0, Tick, Tref, Tc);
+	    reply(From, {ok,State0}),
+	    loop(State0, Tick, Tref, Tc);
 	{call,From,get_tc} ->			%Get the tick count
 	    reply(From, {ok,Tc}),
-	    loop(St0, Tick, Tref, Tc);
+	    loop(State0, Tick, Tref, Tc);
 	{cast,From,{set_unit,Name}} ->		%Set a new unit chunk
-	    {_,St1} = do_set_unit(Name, Tick, St0),
-	    loop(St1, Tick, Tref, Tc);
-	{call,From,{lua_do,Cmd}} ->		%"do" any Lua command
-	    {Rs,St1} = luerl:do(Cmd, St0),
+	    {_,State1} = do_set_unit(Name, Tick, State0),
+	    loop(State1, Tick, Tref, Tc);
+	{call,From,{lua_do,Command}} ->		%"do" any Lua command
+	    {Rs,State1} = luerl:do(Command, State0),
 	    reply(From, {ok,Rs}),
-	    loop(St1, Tick, Tref, Tc);
+	    loop(State1, Tick, Tref, Tc);
 	{call,From,gc} ->			%Gc the luerl state
-	    St1 = luerl:gc(St0),
+	    State1 = luerl:gc(State0),
 	    reply(From, ok),
-	    loop(St1, Tick, Tref, Tc)
+	    loop(State1, Tick, Tref, Tc)
     end.
 
 %% do_set_unit(UnitFileName, Tick, LuerlState) -> LuerlState.
@@ -141,12 +139,12 @@ loop(St0, Tick, Tref, Tc) ->
 %%  reload an updated unit. Get the existing position and speed and
 %%  insert these into the new chunk.
 
-do_set_unit(Name, Tick, St0) ->
-    {[X,Y],St1} = luerl:call_function([this_unit,get_position], [], St0),
-    {[Dx,Dy],St2} = luerl:call_function([this_unit,get_speed], [], St1),
-    {_,St3} = luerl:do("this_unit = require '" ++ Name ++ "'", St2),
-    {_,St4} = luerl:call_function([this_unit,start], [], St3),
-    {_,St5} = luerl:call_function([this_unit,set_position], [X,Y], St4),
-    {_,St6} = luerl:call_function([this_unit,set_speed], [Dx,Dy], St5),
-    {Rs,St7} = luerl:call_function([this_unit,set_tick], [Tick], St6),
-    {Rs,St7}.
+do_set_unit(Name, Tick, State0) ->
+    {[X,Y],State1} = luerl:call_function([this_unit,get_position], [], State0),
+    {[Dx,Dy],State2} = luerl:call_function([this_unit,get_speed], [], State1),
+    {_,State3} = luerl:do("this_unit = require '" ++ Name ++ "'", State2),
+    {_,State4} = luerl:call_function([this_unit,start], [], State3),
+    {_,State5} = luerl:call_function([this_unit,set_position], [X,Y], State4),
+    {_,State6} = luerl:call_function([this_unit,set_speed], [Dx,Dy], State5),
+    {Rs,State7} = luerl:call_function([this_unit,set_tick], [Tick], State6),
+    {Rs,State7}.
