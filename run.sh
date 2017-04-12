a9302f501e44144fd57529736edfa251a8c69013
Jean Chassoul <chassoul@gmail.com>
2017-04-11 20:15:32 -0400

    enable observer by default

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 DEMO_LIBS="./deps/*/ebin ./ebin"
 
-# observer
-exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -pa $DEMO_LIBS
+exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -pa $DEMO_LIBS -run observer
