8e72b150e04d0df6ef18488718155a134d1a8732
Jean Chassoul <chassoul@gmail.com>
2017-07-13 01:10:46 -0400

    no

--- treehouse/structures/__init__.py
+++ treehouse/structures/__init__.py
@@ -4,7 +4,7 @@
 
     Nodes allow control of additional CPU and GPU units.
 
-    Nodes provide control for your cloud forest. 
+    Nodes provide control for your cloud forest.
     As your forces grow in number, you must spawn more nodes to control them.
 '''
 
