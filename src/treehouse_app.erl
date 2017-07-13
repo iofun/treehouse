1d3453916a09c66d768a5b711fe1a17ce0287b83
Jean Chassoul <chassoul@gmail.com>
2017-07-13 18:50:28 -0400

    ok

--- src/treehouse_app.erl
+++ src/treehouse_app.erl
@@ -5,16 +5,16 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/units/", units_handler, []},
-            {"/schemes/", schemes_handler, []},
-            {"/structures/", structures_handler, []}  
-        ]}
-    ]),
-    {ok, _} = cowboy:start_clear(http, 100, [{port, 8215}], #{
-        env => #{dispatch => Dispatch}}
-    ),
+    % Dispatch = cowboy_router:compile([
+    %     {'_', [
+    %         {"/units/", units_handler, []},
+    %         {"/schemes/", schemes_handler, []},
+    %         {"/structures/", structures_handler, []}  
+    %     ]}
+    % ]),
+    % {ok, _} = cowboy:start_clear(http, 100, [{port, 8215}], #{
+    %     env => #{dispatch => Dispatch}}
+    % ),
     {ok, _} = zmq_sub_bind:start_link(),
     treehouse_sup:start_link().
 
