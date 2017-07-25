e39449b5b7bb694ad2f0110f5c8c8c0471a9ebe3
Jean Chassoul <chassoul@gmail.com>
2017-07-25 10:14:56 -0400

    no

--- debug.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-DEMO_LIBS="./deps/*/ebin ./ebin"
-
-# -run observer: WX Failed loading "wxe_driver"@"/usr/lib/erlang/lib/wx-1.8/priv"
-
-exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -pa $DEMO_LIBS
