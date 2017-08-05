246fa556494f7dbf6c68542f64a010cb9559cf80
Jean Chassoul <chassoul@gmail.com>
2017-08-04 20:50:26 -0400

    ok

--- src/zmq.erl
+++ src/zmq.erl
@@ -27,7 +27,6 @@ start_link() ->
 
 socket(SocketType, SocketOptions) ->
     io:format("socket options ~p ~p \n", [SocketType, SocketOptions]),
-    lager:warning("socket options ~p ~p \n", [SocketType, SocketOptions]),
     call({socket,SocketType,SocketOptions}).
 
 connect(Address) ->
