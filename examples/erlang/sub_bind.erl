a0dd01fc073e5cb9177f15ed1cf1fa13a91d3a7c
ooo <ooo@erebus>
2017-12-09 18:07:18 -0500

    Yo, WTF?

--- examples/erlang/sub_bind.erl
+++ examples/erlang/sub_bind.erl
@@ -6,11 +6,9 @@ main(_) ->
 	io:format("Subscriber bind treehouse OTP release erlang escript.\n",[]),
     application:ensure_all_started(chumak),
     {ok, Socket} = chumak:socket(sub),
-
     %% Subscribe topic
     Heartbeat = <<"heartbeat">>,
     chumak:subscribe(Socket, Heartbeat),
-    
     case chumak:bind(Socket, tcp, "localhost", 5813) of
         {ok, _BindPid} ->
             io:format("Binding OK with Pid: ~p\n", [Socket]);
--- units/debug.sh
+++ units/debug.sh
@@ -2,6 +2,4 @@
 
 DEMO_LIBS="../deps/*/ebin ../ebin"
 
-# -run observer: WX Failed loading "wxe_driver"@"/usr/lib/erlang/lib/wx-1.8/priv"
-
 exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -pa $DEMO_LIBS
