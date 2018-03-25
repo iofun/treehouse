db0cf64c895d5e1e2a72d954b829b761b9ee62d4
Jean Chassoul <chassoul@gmail.com>
2018-03-25 19:55:01 -0400

    clean, clean, clean

--- units/valkyrie_unit.lua
+++ units/valkyrie_unit.lua
@@ -1,19 +1,38 @@
--- The doggo unit.
--- It reflects our lovely zergling style.
+-- The Valkyrie is a Terran air-to-air unit that is available to be built 
+-- from a Starport once both an Armory and a Control Tower are on the field.
 
-local this_unit = {}                -- Our unit function table
+-- Our unit function table
+local this_unit = {}                
 
     -- The standard local variables
-    local x, y, dx, dy                  -- Where we are and fast we move
-    local color = "white"               -- Our color
-    local style = "unit"                -- Our style
-    local tick                          -- Size of a clock tick msec
-    local me = unit.self()              -- This is me
-    local ammo,shield = 0,0
     
-    local xsize,ysize = region.size()   -- The size of the region
+    -- Where we are and fast we move
+    local x, y, dx, dy
+    -- Our color               
+    local color = "blue"
+    -- Our style           
+    local style = "unit"
+    -- Size of a clock tick msec
+    local tick
     
-    -- The doggo unit interface.
+    -- This is me              
+    local me = unit.self()
+
+    -- and now for something completely different
+    local label = "large_air"
+    local hitpoints,shield = 200,2
+    local ground_damage,air_damage = 0,8
+    local ground_cooldown, air_cooldown = 0,2.688
+    local sight,range = 8,6
+    local supply = 3
+    local cooldown = 32
+    local gold = 250
+    local weed = 125
+
+    -- The size of the region
+    local xsize,ysize = region.size()
+    
+    -- The unit interface.
     
     function this_unit.start() end
     
@@ -30,11 +49,13 @@ local this_unit = {}                -- Our unit function table
     local function move_xy_bounce(x, y, dx, dy, valid_x, valid_y)
        local nx = x + dx
        local ny = y + dy
-       if (not valid_x(nx)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_x(nx)) then
           nx = x - dx
           dx = -dx
        end
-       if (not valid_y(ny)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_y(ny)) then
           ny = y - dy
           dy = -dy
        end
@@ -59,9 +80,10 @@ local this_unit = {}                -- Our unit function table
        x,y,dx,dy = move(x, y, dx, dy)
     end
     
-    function this_unit.attack()        -- The unit has been zapped and will die
+    function this_unit.attack()
+       -- The unit has been zapped and will die
        region.rem_sector(x, y)
     end
     
-    return this_unit                   -- Return the unit table
-    
+    -- Return the unit table
+    return this_unit
--- units/vulture_unit.lua
+++ units/vulture_unit.lua
@@ -1,19 +1,37 @@
--- The doggo unit.
--- It reflects our lovely zergling style.
+-- The Vulture is an extremely mobile and dangerous Terran unit produced from the Factory.
 
-local this_unit = {}                -- Our unit function table
+-- Our unit function table
+local this_unit = {}                
 
     -- The standard local variables
-    local x, y, dx, dy                  -- Where we are and fast we move
-    local color = "white"               -- Our color
-    local style = "unit"                -- Our style
-    local tick                          -- Size of a clock tick msec
-    local me = unit.self()              -- This is me
-    local ammo,shield = 0,0
     
-    local xsize,ysize = region.size()   -- The size of the region
+    -- Where we are and fast we move
+    local x, y, dx, dy
+    -- Our color               
+    local color = "blue"
+    -- Our style           
+    local style = "unit"
+    -- Size of a clock tick msec
+    local tick
     
-    -- The doggo unit interface.
+    -- This is me              
+    local me = unit.self()
+
+    -- and now for something completely different
+    local label = "medium_ground"
+    local hitpoints,shield = 80,0
+    local ground_damage,air_damage = 20,0
+    local ground_cooldown, air_cooldown = 1.26,0
+    local sight,range = 8,5
+    local supply = 2
+    local cooldown = 19
+    local gold = 75
+    local weed = 0
+
+    -- The size of the region
+    local xsize,ysize = region.size()
+    
+    -- The unit interface.
     
     function this_unit.start() end
     
@@ -30,11 +48,13 @@ local this_unit = {}                -- Our unit function table
     local function move_xy_bounce(x, y, dx, dy, valid_x, valid_y)
        local nx = x + dx
        local ny = y + dy
-       if (not valid_x(nx)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_x(nx)) then
           nx = x - dx
           dx = -dx
        end
-       if (not valid_y(ny)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_y(ny)) then
           ny = y - dy
           dy = -dy
        end
@@ -59,9 +79,10 @@ local this_unit = {}                -- Our unit function table
        x,y,dx,dy = move(x, y, dx, dy)
     end
     
-    function this_unit.attack()        -- The unit has been zapped and will die
+    function this_unit.attack()
+       -- The unit has been zapped and will die
        region.rem_sector(x, y)
     end
     
-    return this_unit                   -- Return the unit table
-    
+    -- Return the unit table
+    return this_unit
