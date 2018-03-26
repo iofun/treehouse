8783eb9a0cabd07d3a2139e274c29901af762936
Jean Chassoul <chassoul@gmail.com>
2018-03-26 00:22:29 -0400

    clean, clean, clean

--- units/infested_unit.lua
+++ units/infested_unit.lua
@@ -1,19 +1,39 @@
--- The doggo unit.
--- It reflects our lovely zergling style.
+-- The Infested is a Zerg suicide unit produced from the Infested Command,
+-- which is created by Queens from damaged Command Centers.
 
-local this_unit = {}                -- Our unit function table
+-- Our unit function table
+local this_unit = {}
 
     -- The standard local variables
-    local x, y, dx, dy                  -- Where we are and fast we move
-    local color = "white"               -- Our color
-    local style = "unit"                -- Our style
-    local tick                          -- Size of a clock tick msec
-    local me = unit.self()              -- This is me
-    local ammo,shield = 0,0
     
-    local xsize,ysize = region.size()   -- The size of the region
+    -- Where we are and fast we move
+    local x, y, dx, dy
+    -- Our color
+    local color = "red"
+    -- Our style
+    local style = "unit"
+    -- Size of a clock tick msec
+    local tick
     
-    -- The doggo unit interface.
+    -- This is me              
+    local me = unit.self()
+
+    -- and now for something completely different
+    local label = "small_ground"
+    local hitpoints,shield = 60,0
+    local ground_damage,air_damage = 500,0
+    local ground_cooldown, air_cooldown = 0,0
+    local ground_range, air_range = 1,0
+    local sight = 5
+    local supply = 1
+    local cooldown = 25
+    local gold = 100
+    local weed = 50
+
+    -- The size of the region
+    local xsize,ysize = region.size()
+    
+    -- The unit interface.
     
     function this_unit.start() end
     
@@ -30,11 +50,13 @@ local this_unit = {}                -- Our unit function table
     local function move_xy_bounce(x, y, dx, dy, valid_x, valid_y)
        local nx = x + dx
        local ny = y + dy
-       if (not valid_x(nx)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_x(nx)) then
           nx = x - dx
           dx = -dx
        end
-       if (not valid_y(ny)) then       -- Bounce off the edge
+       -- Bounce off the edge
+       if (not valid_y(ny)) then
           ny = y - dy
           dy = -dy
        end
@@ -59,9 +81,10 @@ local this_unit = {}                -- Our unit function table
        x,y,dx,dy = move(x, y, dx, dy)
     end
     
-    function this_unit.attack()        -- The unit has been zapped and will die
+    function this_unit.attack()
+       -- The unit has been zapped and will die
        region.rem_sector(x, y)
     end
     
-    return this_unit                   -- Return the unit table
-    
+    -- Return the unit table
+    return this_unit
