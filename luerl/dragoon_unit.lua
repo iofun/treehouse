60c2f9e5b3c0b3a68efa3f8e4f1a404248ee373d
Jean Chassoul <chassoul@gmail.com>
2018-03-28 18:29:04 -0400

    clean, clean, clean

--- luerl/dragoon_unit.lua
+++ luerl/dragoon_unit.lua
@@ -1,4 +1,4 @@
--- The Hydralisk is a versatile Zerg ranged unit that can attack both air and ground targets using needle spines.  
+-- The Dragoon is a ranged ground unit that can be warped in at the Protoss Gateway if a Cybernetics Core has been warped in.
 
 -- Our unit function table
 local this_unit = {}
@@ -8,7 +8,7 @@ local this_unit = {}
     -- Where we are and fast we move
     local x, y, dx, dy
     -- Our color
-    local color = "red"
+    local color = "green"
     -- Our style
     local style = "unit"
     -- Size of a clock tick msec
@@ -18,16 +18,17 @@ local this_unit = {}
     local me = unit.self()
 
     -- and now for something completely different
-    local label = "medium_ground"
-    local hitpoints,shield = 80,0
-    local ground_damage,air_damage = 10,10
-    local ground_cooldown, air_cooldown = 0.63,0.63
+    local label = "large_ground"
+    local armor = 1
+    local hitpoints,shield = 100,80
+    local ground_damage,air_damage = 20,20
+    local ground_cooldown, air_cooldown = 0.924,0
     local ground_range, air_range = 4,4
-    local sight = 6
-    local supply = 1
-    local cooldown = 18
-    local gold = 75
-    local weed = 25
+    local sight = 8
+    local supply = 2
+    local cooldown = 32
+    local gold = 125
+    local weed = 50
 
     -- The size of the region
     local xsize,ysize = region.size()
