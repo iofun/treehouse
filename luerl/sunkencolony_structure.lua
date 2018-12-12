40dafd80b6180305a0f6cdb6d552bddd109cd2fa
Jean Chassoul <jchassoul@gmail.com>
2018-12-12 13:00:00 -0600

    clean, clean, clean

--- luerl/sunkencolony_structure.lua
+++ luerl/sunkencolony_structure.lua
@@ -1,67 +1,89 @@
--- The doggo unit.
--- It reflects our lovely zergling type.
+-- The Sunken Colony requires Spawning Pool technology 
+-- before it can be morphed from Creep Colony.
 
-local this_unit = {}                -- Our unit function table
+-- Our unit function table
+   local this_unit = {}
 
-    -- The standard local variables
-    local x, y, dx, dy                  -- Where we are and fast we move
-    local color = "white"               -- Our color
-    local type = "unit"                -- Our type
-    local tick                          -- Size of a clock tick msec
-    local me = unit.self()              -- This is me
-    local ammo,shield = 0,0
-    
-    local xsize,ysize = region.size()   -- The size of the region
-    
-    -- The doggo unit interface.
-    
-    function this_unit.start() end
-    
-    function this_unit.get_position() return x,y end
-    
-    function this_unit.set_position(a1, a2) x,y = a1,a2 end
-    
-    function this_unit.get_speed() return dx,dy end
-    
-    function this_unit.set_speed(a1, a2) dx,dy = a1,a2 end
-    
-    function this_unit.set_tick(a1) tick = a1 end
-    
-    local function move_xy_bounce(x, y, dx, dy, valid_x, valid_y)
-       local nx = x + dx
-       local ny = y + dy
-       if (not valid_x(nx)) then       -- Bounce off the edge
-          nx = x - dx
-          dx = -dx
-       end
-       if (not valid_y(ny)) then       -- Bounce off the edge
-          ny = y - dy
-          dy = -dy
-       end
-       return nx, ny, dx, dy
-    end
-    
-    local function move(x, y, dx, dy)
-       local nx,ny,ndx,ndy = move_xy_bounce(x, y, dx, dy,
-                   region.valid_x, region.valid_y)
-       -- Where we were and where we are now.
-       local osx,osy = region.sector(x, y)
-       local nsx,nsy = region.sector(nx, ny)
-       if (osx ~= nsx or osy ~= nsy) then
-          -- In new sector, move us to the right sector
-          region.rem_sector(x, y)
-          region.add_sector(nx, ny)
-       end
-       return nx,ny,ndx,ndy
-    end
-    
-    function this_unit.tick()
-       x,y,dx,dy = move(x, y, dx, dy)
-    end
-    
-    function this_unit.attack()        -- The unit has been zapped and will die
-       region.rem_sector(x, y)
-    end
-    
-    return this_unit                   -- Return the unit table
-    
+   -- Where we are and fast we move
+   local x, y, dx, dy
+   -- Our color               
+   local color = "red"
+   -- Our label           
+   local label = "zerg_structure"
+   -- Size of a clock tick msec
+   local tick
+   
+   -- It's me, the unit structure 
+   local me = unit.self()
+
+   -- The standard local variables
+   local armor = 2
+   local hitpoints,shield = 300,0
+   local ground_damage,air_damage = 40,0
+   local ground_cooldown, air_cooldown = 1.344,0
+   local ground_range, air_range = 7,0
+   local sight = 10
+   local supply = 0
+   local cooldown = 12
+   local mineral = 50
+   local gas = 0
+   local holdkey = "u"
+
+   -- The size of the region
+   local xsize,ysize = region.size()
+   
+   -- The unit interface.
+   
+   function this_unit.start() end
+   
+   function this_unit.get_position() return x,y end
+   
+   function this_unit.set_position(a1, a2) x,y = a1,a2 end
+   
+   function this_unit.get_speed() return dx,dy end
+   
+   function this_unit.set_speed(a1, a2) dx,dy = a1,a2 end
+   
+   function this_unit.set_tick(a1) tick = a1 end
+   
+   local function move_xy_bounce(x, y, dx, dy, valid_x, valid_y)
+      local nx = x + dx
+      local ny = y + dy
+      -- Bounce off the edge
+      if (not valid_x(nx)) then
+         nx = x - dx
+         dx = -dx
+      end
+      -- Bounce off the edge
+      if (not valid_y(ny)) then
+         ny = y - dy
+         dy = -dy
+      end
+      return nx, ny, dx, dy
+   end
+   
+   local function move(x, y, dx, dy)
+      local nx,ny,ndx,ndy = move_xy_bounce(x, y, dx, dy,
+                  region.valid_x, region.valid_y)
+      -- Where we were and where we are now.
+      local osx,osy = region.sector(x, y)
+      local nsx,nsy = region.sector(nx, ny)
+      if (osx ~= nsx or osy ~= nsy) then
+         -- In new sector, move us to the right sector
+         region.rem_sector(x, y)
+         region.add_sector(nx, ny)
+      end
+      return nx,ny,ndx,ndy
+   end
+   
+   function this_unit.tick()
+      x,y,dx,dy = move(x, y, dx, dy)
+   end
+   
+   function this_unit.attack()
+      -- The unit has been zapped and will die
+      region.rem_sector(x, y)
+   end
+   
+   -- Return the unit table
+   return this_unit
