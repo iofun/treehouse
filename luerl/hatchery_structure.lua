219f0887688125c2706d13ded01c383a97300f7d
Jean Chassoul <jchassoul@gmail.com>
2018-12-22 10:22:37 -0600

    clean, clean, clean

--- luerl/hatchery_structure.lua
+++ luerl/hatchery_structure.lua
@@ -3,27 +3,27 @@
 
 -- Our unit function table
 local this_unit = {}
-
 -- Where we are and fast we move
 local x, y, dx, dy
 -- Our color               
 local color = "red"
 -- Our label           
 local label = "zerg_structure"
+-- Our category
+local category = "basic_building"
 -- Size of a clock tick msec
 local tick
-
 -- It's me, the unit structure 
 local me = unit.self()
-
 -- The standard local variables
 local armor = 1
 local hitpoints,shield = 1250,0
 local ground_damage,air_damage = 0,0
-local ground_cooldown, air_cooldown = 0,0
-local ground_range, air_range = 0,0
+local ground_cooldown,air_cooldown = 0,0
+local ground_range,air_range = 0,0
 local sight = 8
-local supply = 1
+local speed = 0
+local supply = 0 
 local cooldown = 75
 local mineral = 300
 local gas = 0
@@ -33,7 +33,6 @@ local holdkey = "h"
 local xsize,ysize = region.size()
 
 -- The unit interface.
-
 function this_unit.start() end
 
 function this_unit.get_position() return x,y end
