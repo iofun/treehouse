b33eebce0f0c593404093f1017bcd096a87fa7d8
Jean Chassoul <jchassoul@gmail.com>
2018-12-22 10:03:40 -0600

    clean, clean, clean

--- luerl/extractor_structure.lua
+++ luerl/extractor_structure.lua
@@ -2,26 +2,26 @@
 
 -- Our unit function table
 local this_unit = {}
-
 -- Where we are and fast we move
 local x, y, dx, dy
 -- Our color               
 local color = "red"
 -- Our label           
 local label = "zerg_structure"
+-- Our category
+local category = "basic_building"
 -- Size of a clock tick msec
 local tick
-
 -- It's me, the unit structure 
 local me = unit.self()
-
 -- The standard local variables
 local armor = 1
 local hitpoints,shield = 750,0
 local ground_damage,air_damage = 0,0
-local ground_cooldown, air_cooldown = 0,0
-local ground_range, air_range = 0,0
+local ground_cooldown,air_cooldown = 0,0
+local ground_range,air_range = 0,0
 local sight = 8
+local speed = 0
 local supply = 0
 local cooldown = 25
 local mineral = 50
@@ -32,7 +32,6 @@ local holdkey = "e"
 local xsize,ysize = region.size()
 
 -- The unit interface.
-
 function this_unit.start() end
 
 function this_unit.get_position() return x,y end
