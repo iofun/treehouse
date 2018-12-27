ce6da86d0d93ca14d9191529d12e5cbcf8ce69f7
Jean Chassoul <jchassoul@gmail.com>
2018-12-26 19:49:10 -0600

    clean, clean, clean

--- daemons.d
+++
@@ -1,4 +0,0 @@
-src/luerl_region.erl:: /home/ooo/work/daemons/deps/luerl/src/luerl.hrl; @touch $@
-src/luerl_unit.erl:: /home/ooo/work/daemons/deps/luerl/src/luerl.hrl; @touch $@
-
-COMPILE_FIRST +=
