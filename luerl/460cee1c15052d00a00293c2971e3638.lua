7bc04d4c0534c7308b916e15fd99b6c7a5a38928
Jean Chassoul <jchassoul@gmail.com>
2020-02-08 06:06:28 -0600

    clean, clean, clean

--- luerl/460cee1c15052d00a00293c2971e3638.lua
+++ luerl/460cee1c15052d00a00293c2971e3638.lua
@@ -4,11 +4,11 @@ local this_unit = {}
 -- Where we are and fast we move
 local x, y, dx, dy
 -- Our name
-local name = "Protoss_Arbiter_Tribunal"
--- Our color              
+local name = "Protoss_Observatory"
+-- Our color 
 local color = "yellow"
--- Our BWAPI unit type          
-local type = 170
+-- Our BWAPI unit type 
+local type = 159
 -- Size of a clock tick msec
 local tick
 
@@ -18,17 +18,17 @@ local me = unit.self()
 -- The standard local variables
 local label = "large_structure"
 local armor = 1
-local hitpoints,shield = 500,500
+local hitpoints,shield = 250,250
 local ground_damage,air_damage = 0,0
 local ground_cooldown,air_cooldown = 0,0
 local ground_range,air_range = 0,0
 local sight = 320
 local supply = 0
-local build_time = 900
-local build_score = 450
-local destroy_score = 1350
-local mineral = 200
-local gas = 150
+local build_time = 450 
+local build_score = 175
+local destroy_score = 525 
+local mineral = 50
+local gas = 100 
 local holdkey = ""
 
 -- The size of the region
--- luerl/b06e5483c17ef29e01a0ec995c0535de.lua
+++ luerl/b06e5483c17ef29e01a0ec995c0535de.lua
@@ -4,11 +4,11 @@ local this_unit = {}
 -- Where we are and fast we move
 local x, y, dx, dy
 -- Our name
-local name = "Protoss_Arbiter_Tribunal"
--- Our color              
+local name = "Protoss_Photon_Cannon"
+-- Our color 
 local color = "yellow"
--- Our BWAPI unit type          
-local type = 170
+-- Our BWAPI unit type
+local type = 162
 -- Size of a clock tick msec
 local tick
 
@@ -18,17 +18,17 @@ local me = unit.self()
 -- The standard local variables
 local label = "large_structure"
 local armor = 1
-local hitpoints,shield = 500,500
+local hitpoints,shield = 100,100
 local ground_damage,air_damage = 0,0
 local ground_cooldown,air_cooldown = 0,0
 local ground_range,air_range = 0,0
-local sight = 320
+local sight = 352
 local supply = 0
-local build_time = 900
-local build_score = 450
-local destroy_score = 1350
-local mineral = 200
-local gas = 150
+local build_time = 750
+local build_score = 100
+local destroy_score = 300
+local mineral = 150
+local gas = 0
 local holdkey = ""
 
 -- The size of the region
